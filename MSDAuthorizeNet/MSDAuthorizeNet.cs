4c659dc55a1654f556e2c8ab0a2bcb3fbe73b533
Edgar Mendez <edgar.mendez@gmail.com>
2012-03-01 12:07:38 -0700

    Added MSDAuthorizeNet provider

---
+++ MSDAuthorizeNet/MSDAuthorizeNet.cs
@@ -0,0 +1,13 @@
+﻿using System;
+using System.Collections.Generic;
+using AuthorizeNet;
+
+
+namespace Ektron.Cms.Extensibility.Commerce.MSD
+{
+    public class MSDAuthorizeNet
+    {
+       
+
+    }
+}
